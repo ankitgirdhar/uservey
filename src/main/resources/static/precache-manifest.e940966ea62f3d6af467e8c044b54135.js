93db5b754d72a5d58abbb69a58e51e54c81a9d93
ankitgirdhar <stanleycooper136@gmail.com>
2020-07-29 18:48:53 +0530

    added frontend build files

---
+++ src/main/resources/static/precache-manifest.e940966ea62f3d6af467e8c044b54135.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e461726c74c413267b86543570fd8d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b860b7dc2c2885a03f13",
+    "url": "/static/css/main.7f684076.chunk.css"
+  },
+  {
+    "revision": "95b496828e22b5be2c90",
+    "url": "/static/js/2.e4e8597e.chunk.js"
+  },
+  {
+    "revision": "fdb2bbbbb647ba518ef54493c5cba538",
+    "url": "/static/js/2.e4e8597e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b860b7dc2c2885a03f13",
+    "url": "/static/js/main.5a0d037f.chunk.js"
+  },
+  {
+    "revision": "5793271eb2cbb1a234be",
+    "url": "/static/js/runtime-main.e58a2a7d.js"
+  },
+  {
+    "revision": "547770381110487d64133cc95804b715",
+    "url": "/static/media/paper.54777038.png"
+  }
+]);
